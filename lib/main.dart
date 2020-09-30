55d3cd8063504869fec58164687ac77f1e78a5c8
rejathxyz <tmrejath@gmail.com>
2020-09-30 20:49:09 +0530

    Sample

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Material App",
+      title: "Note Keeper",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
